c03d57d8390df39237044e979828ac79c697a1f4
szeke <szekely1401@gmail.com>
2014-07-30 20:37:09 -0700

    cora RDF with uris based on uuids

--- k/python/scripts.py
+++ k/python/scripts.py
@@ -1,7 +1,9 @@
 import hashlib
+import uuid
 
 def getMD5Hash(x):
 	return hashlib.md5(x).hexdigest();
 
 def paperUri():
-	return "person/"+getMD5Hash(getValue("authors")+getValue("title")+getValue("affiliation")+getValue("venue")+getValue("address")+getValue("publishers")+getValue("year")+getValue("pages"));
+#	return "person/"+getMD5Hash(getValue("authors")+getValue("volume")+getValue("title")+getValue("affiliation")+getValue("venue")+getValue("address")+getValue("publishers")+getValue("year")+getValue("pages")+getValue("editor")+getValue("note")+getValue("month"));
+	return "person/uuid/"+str(uuid.uuid4());
